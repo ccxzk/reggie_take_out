d9475c16e8570c1e04f65bd888ff3e6acfda39ff
OWO <2118793916@qq.com>
2025-05-24 22:25:30 +0800

    增加用户登录、退出登录功能

--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
@@ -19,7 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/employee")
 public class EmployeeController {
 
-    @Autowired
+    @Resource
     private EmployeeService employeeService;
 
     /**
@@ -30,34 +31,30 @@ public class EmployeeController {
      */
     @PostMapping("/login")
     public R<Employee> login(HttpServletRequest request,@RequestBody Employee employee){
-
-        //1、将页面提交的密码password进行md5加密处理
+        //将前端前来的用户密码明文进行md5加密
         String password = employee.getPassword();
         password = DigestUtils.md5DigestAsHex(password.getBytes());
 
-        //2、根据页面提交的用户名username查询数据库
+        //查询用户是否存在
         LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(Employee::getUsername,employee.getUsername());
         Employee emp = employeeService.getOne(queryWrapper);
 
-        //3、如果没有查询到则返回登录失败结果
-        if(emp == null){
-            return R.error("登录失败");
+        //用户不存在情况
+        if (emp == null){
+           return R.error("用户不存在");
         }
 
-        //4、密码比对，如果不一致则返回登录失败结果
-        if(!emp.getPassword().equals(password)){
-            return R.error("登录失败");
+        //用户存在情况
+        if(emp.getPassword().equals(password)){
+            //密码正确
+            request.getSession().setAttribute("employee",emp.getId()); //将用户id存入session，以便退出登录
+            return R.success(emp);
+        }else{
+            //密码错误
+            return R.error("密码错误");
         }
 
-        //5、查看员工状态，如果为已禁用状态，则返回员工已禁用结果
-        if(emp.getStatus() == 0){
-            return R.error("账号已禁用");
-        }
-
-        //6、登录成功，将员工id存入Session并返回登录成功结果
-        request.getSession().setAttribute("employee",emp.getId());
-        return R.success(emp);
     }
 
     /**
@@ -67,7 +64,6 @@ public class EmployeeController {
      */
     @PostMapping("/logout")
     public R<String> logout(HttpServletRequest request){
-        //清理Session中保存的当前登录员工的id
         request.getSession().removeAttribute("employee");
         return R.success("退出成功");
     }
